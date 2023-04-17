1ec3d27b800807592e0e906702c9ba5c6b0dc1f1
NyIIIa <nedviga.ivan2003@gmail.com>
2023-04-17 14:49:43 +0300

    add infrastructure dependency injection

---
+++ src/GasStation.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,52 @@
+using System.Text;
+using GasStation.Application.Common.Interfaces.Authentication;
+using GasStation.Application.Common.Interfaces.Services;
+using GasStation.Infrastructure.Authentication;
+using GasStation.Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace GasStation.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
+    {
+        services.AddAuth(configuration);
+        
+        services.AddSingleton<IDateTimeService, DateTimeService>();
+
+        services.AddAuthentication();   
+        
+        return services;
+    }
+    
+    public static IServiceCollection AddAuth(
+        this IServiceCollection services,
+        ConfigurationManager configuration)
+    {
+        var jwtSettings = new JwtSettings();
+        configuration.Bind(JwtSettings.SectionName, jwtSettings);
+
+        services.AddSingleton(Options.Create(jwtSettings));
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+
+        services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings.Issuer,
+                ValidAudience = jwtSettings.Audience,
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(jwtSettings.Secret))
+            });
+
+        return services;
+    }
+}
