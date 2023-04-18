d5c8b2b675b08ed323720baa6b3a5214316f44da
NyIIIa <nedviga.ivan2003@gmail.com>
2023-04-18 15:37:45 +0300

    add design time db context factory

---
+++ src/GasStation.Infrastructure/Context/DesignTimeDbContextFactoryBase.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace GasStation.Infrastructure.Context;
+
+public abstract class DesignTimeDbContextFactoryBase<TContext> : IDesignTimeDbContextFactory<TContext>
+    where TContext : DbContext
+{
+    protected abstract TContext CreateNewInstance(DbContextOptions<TContext> options);
+    public TContext CreateDbContext(string[] args)
+    {
+        var basePath = Directory.GetCurrentDirectory() + string.Format("{0}..{0}GasStation.WebApi", Path.DirectorySeparatorChar);
+            
+        DbContextOptionsBuilder<TContext> builder = new DbContextOptionsBuilder<TContext>();
+        IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(basePath)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        builder.UseSqlServer(configuration.GetConnectionString(ConnectionString.DefaultSectionName));
+        return CreateNewInstance(builder.Options);
+    }
+}
