142ee279289a3f057e1e2ff322103af196e892d3
NyIIIa <nedviga.ivan2003@gmail.com>
2023-04-18 13:15:27 +0300

    implement error controller

---
+++ src/GasStation.WebApi/Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GasStation.WebApi.Controllers;
+
+[Route("/error")]
+public class ErrorController : ControllerBase
+{
+    public IActionResult Error()
+    {
+        var exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+        return Problem(title: exception?.Message);
+    }
+}
