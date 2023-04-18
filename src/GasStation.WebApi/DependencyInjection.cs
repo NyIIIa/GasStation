ce471689366a0e3c9b0a4a8649d03d3ce7cec331
NyIIIa <nedviga.ivan2003@gmail.com>
2023-04-18 13:10:26 +0300

    setup Program.cs class

---
+++ src/GasStation.WebApi/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿namespace GasStation.WebApi;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddSwaggerGen();
+        
+        return services;
+    }
+}
--- src/GasStation.WebApi/Program.cs
+++ src/GasStation.WebApi/Program.cs
@@ -1,25 +1,28 @@
+using GasStation.Application;
+using GasStation.Infrastructure;
+using GasStation.WebApi;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services
+        .AddPresentation()
+        .AddApplication()
+        .AddInfrastructure(builder.Configuration);
 }
 
-app.UseHttpsRedirection();
+var app = builder.Build();
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+    app.UseExceptionHandler("/error");
+        
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
+   
+}
