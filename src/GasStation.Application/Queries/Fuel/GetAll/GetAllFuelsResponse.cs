2ea3af33d52f6c521519a37cd1b7d25e23fb6e55
NyIIIa <nedviga.ivan2003@gmail.com>
2023-05-10 14:55:26 +0300

    updated mapping Fuel => GetAllFuelsResponse

--- src/GasStation.Application/Queries/Fuel/GetAll/GetAllFuelsResponse.cs
+++ src/GasStation.Application/Queries/Fuel/GetAll/GetAllFuelsResponse.cs
@@ -2,8 +2,9 @@ namespace GasStation.Application.Queries.Fuel.GetAll;
 
 public class GetAllFuelsResponse
 {
+    public int Id { get; set; }
     public string Title { get; set; } = null!;
     public double Quantity { get; set; }
     public decimal Price { get; set; }
-    public long PriceChaneDate { get; set; }
+    public string PriceChangeDate { get; set; } = null!;
 }
