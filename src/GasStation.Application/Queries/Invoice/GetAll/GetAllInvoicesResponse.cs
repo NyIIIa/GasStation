5e24fc67685f18d8b992f6e23a2dc95a3ae46842
NyIIIa <nedviga.ivan2003@gmail.com>
2023-05-10 14:57:47 +0300

    updated mapping Invoice => GetAllInvoicesResponse

--- src/GasStation.Application/Queries/Invoice/GetAll/GetAllInvoicesResponse.cs
+++ src/GasStation.Application/Queries/Invoice/GetAll/GetAllInvoicesResponse.cs
@@ -4,9 +4,10 @@ namespace GasStation.Application.Queries.Invoice.GetAll;
 
 public class GetAllInvoicesResponse
 {
+    public int Id { get; set; }
     public string Title { get; set; } = null!;
-    public long CreatedDate { get; set; }
-    public TransactionType TransactionType { get; set; }
+    public string CreatedDate { get; set; } = null!;
+    public string TransactionType { get; set; } = null!;
     public string Consumer { get; set; } = null!;
     public string Provider { get; set; } = null!;        
     public decimal TotalPrice { get; set; } 
